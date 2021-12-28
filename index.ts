7982282004ae83d4f164e40fc9e6682ac01e21f8
GreenBudgie <forestengine@yandex.ru>
2021-12-28 17:30:37 +0300

    Add sass support

--- index.ts
+++ index.ts
@@ -2,9 +2,15 @@ import {IncomingMessage} from "http";
 import https from "https";
 import cheerio from "cheerio";
 import express from "express";
+import sassMiddleware from "node-sass-middleware";
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(sassMiddleware({
+    src: "./public",
+    dest: "./public",
+    debug: true
+}));
 app.use(express.static("public"));
 app.listen(3000);
 
