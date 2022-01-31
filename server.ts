00e357c14d4706db7bceca7377f6b318d8bbd829
GreenBudgie <forestengine@yandex.ru>
2022-02-01 00:56:39 +0300

    Heruko port fix

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ app.use(sassMiddleware({
 }));
 app.use(express.static("public"));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
